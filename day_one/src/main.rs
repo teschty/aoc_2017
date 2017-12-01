4ff14b327aa7054764bc9d8bb90d0c3866d249e2
Tyler Tesch <tytesch@gmail.com>
2017-12-01 15:14:35 -0600

    Finished first day

--- day_one/src/main.rs
+++ day_one/src/main.rs
@@ -1,3 +1,36 @@
+use std::fs::File;
+use std::io::Read;
+
+// reads in puzzle.txt
+fn read_file() -> String {
+    let mut f = File::open("puzzle.txt").expect("puzzle.txt couldn't be found :(");
+
+    let mut contents = String::new();
+    f.read_to_string(&mut contents).expect("Couldn't read puzzle.txt :(");
+
+    contents
+}
+
 fn main() {
-    println!("Hello, world!");
+    let puzzle = read_file();
+    // trim to remove newline
+    let puzzle = puzzle.trim();
+    
+    // since these are just all ASCII numbers (0-9)
+    // we can just convert this to byte array
+    let bytes = puzzle.as_bytes();
+
+    let mut sum = 0i32;
+    for i in 0..bytes.len() {
+        // eh, don't like this long conditional
+        if (i + 1 == bytes.len() && bytes[i] == bytes[0]) || bytes[i] == bytes[i + 1] {
+            let value = bytes[i] as i32;
+            // convert to actual value by subtracting ASCII value of '0'
+            let value = value - '0' as i32;
+
+            sum += value;
+        }
+    }
+
+    println!("Captcha solution is {}", sum);
 }
