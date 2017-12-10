1f73965a337decec72a5c012ff6f80707552fca0
Tyler Tesch <tytesch@gmail.com>
2017-12-10 11:14:19 -0600

    Finish day 10 part 2

--- 10/src/main.rs
+++ 10/src/main.rs
@@ -17,11 +17,11 @@ fn part_one(input: &str) -> i32 {
         .map(|s| s.parse().unwrap())
         .collect();
 
-    let mut list = [0i32; LIST_SIZE];
+    let mut list = [0u8; LIST_SIZE];
 
     // initialize with 0..LIST_SIZE
     for i in 0..LIST_SIZE {
-        list[i] = i as i32;
+        list[i] = i as u8;
     }
 
     let mut current_pos = 0;
@@ -39,7 +39,58 @@ fn part_one(input: &str) -> i32 {
         skip_size += 1;
     }
 
-    list[0] * list[1]
+    list[0] as i32 * list[1] as i32
+}
+
+fn part_two(input: &str) -> String {
+    let mut lengths: Vec<usize> = input
+        .as_bytes().iter()
+        .map(|b| usize::from(*b))
+        .collect();
+
+    // add these values for some reason 
+    lengths.extend(&[17, 31, 73, 47, 23]);
+
+    let mut list = [0u8; LIST_SIZE];
+    // initialize with 0..LIST_SIZE
+    for i in 0..LIST_SIZE {
+        list[i] = i as u8;
+    }
+
+    let mut current_pos = 0;
+    let mut skip_size = 0;
+
+    for _ in 0..64 {
+        for length in &lengths {
+            for i in 0..(length / 2) {
+                let start_idx = (current_pos + i) % LIST_SIZE;
+                let end_idx = (current_pos + (length - 1) - i) % LIST_SIZE;
+
+                list.swap(start_idx, end_idx);
+            }
+
+            current_pos += length + skip_size;
+            skip_size += 1;
+        }
+    }
+
+    // make dense hash
+    let mut dense_hash = [0u8; LIST_SIZE / 16];
+    for i in 0..dense_hash.len() {
+        let mut value = 0;
+        for j in 0..16 {
+            value ^= list[i * 16 + j];
+        }
+
+        dense_hash[i] = value;
+    }
+
+    let mut hash_string = String::new();
+    for byte in &dense_hash {
+        hash_string.push_str(&format!("{:x}", byte));
+    }
+
+    hash_string
 }
 
 fn main() {
@@ -47,4 +98,5 @@ fn main() {
     let puzzle = puzzle.trim();
 
     println!("Solution to part one is {}", part_one(puzzle));
+    println!("Solution to part two is {}", part_two(puzzle));
 }
