51947d656f62c0a26b54f6d6236d5fbd6768747a
Tyler Tesch <tytesch@gmail.com>
2017-12-11 09:42:49 -0600

    Finish day 11 part one

---
+++ 11/src/main.rs
@@ -0,0 +1,63 @@
+use std::fs::File;
+use std::io::Read;
+
+fn read_file() -> String {
+    let mut contents = String::new();
+    let mut f = File::open("puzzle.txt").expect("puzzle.txt couldn't be found :(");
+    f.read_to_string(&mut contents).expect("Couldn't read puzzle.txt :(");
+
+    contents
+}
+
+fn part_one(input: &str) -> i32 {
+    let instructions: Vec<&str> = input.split(',').collect();
+
+    let (mut x, mut y) = (0i32, 0i32);
+
+    for instruction in instructions {
+        let even = x % 2 == 0;
+        match instruction {
+            "n" => y += 1,
+            "s" => y -= 1,
+            "nw" => { 
+                y += if even { 1 } else { 0 };
+                x -= 1;
+            }
+            "ne" => { 
+                y += if even { 1 } else { 0 };
+                x += 1;
+            }
+            "sw" => { 
+                y -= if !even { 1 } else { 0 };
+                x -= 1;
+            }
+            "se" => { 
+                y -= if !even { 1 } else { 0 };
+                x += 1;
+            }
+            _ => panic!("Unknown direction {}", instruction)
+        }
+    }
+
+    if x == 0 {
+        y.abs()
+    } else if y == 0 {
+        x.abs()
+    } else {
+        let dx = x.abs();
+        let dy = y.abs();
+
+        if y >= 0 {
+            dx + dy - ((dx as f32 / 2.0).ceil() as i32)
+        } else {
+            dx + dy -  ((dx as f32 / 2.0).floor() as i32)
+        }
+    }
+}
+
+fn main() {
+    let puzzle = read_file();
+    let puzzle = puzzle.trim();
+
+    println!("Solution to part one is {}", part_one(puzzle));
+}
