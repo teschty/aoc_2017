01f7fe7b292bfb39370c021503c4dc258ada78c7
Tyler Tesch <tytesch@gmail.com>
2017-12-13 11:07:41 -0600

    Use fancy functions for fun

--- 13/src/main.rs
+++ 13/src/main.rs
@@ -9,20 +9,6 @@ fn read_file() -> String {
     contents
 }
 
-fn calculate_severity(layers: &Vec<(i32, i32)>, delay: i32) -> (i32, bool) {
-    let mut severity = 0;
-    let mut caught = false;
-
-    for &(depth, range) in layers {
-        if (depth + delay) % (range * 2 - 2) == 0 {
-            caught = true;
-            severity += depth * range; 
-        }
-    }
-
-    (severity, caught)
-}
-
 fn get_layers(input: &str) -> Vec<(i32, i32)> {
     let mut layers = vec![];
 
@@ -40,7 +26,11 @@ fn get_layers(input: &str) -> Vec<(i32, i32)> {
 }
 
 fn part_one(input: &str) -> i32 {
-    calculate_severity(&get_layers(input), 0).0
+    let layers = get_layers(input);
+
+    layers.iter()
+        .filter(|&&(depth, range)| depth % (range * 2 - 2) == 0)
+        .fold(0, |acc, &(depth, range)| acc + (depth * range))
 }
 
 fn part_two(input: &str) -> i32 {
@@ -49,7 +39,9 @@ fn part_two(input: &str) -> i32 {
 
     // might be possible to calculate this in a smart way
     // but let's just brute force it
-    while calculate_severity(&layers, delay).1 { delay += 1; }
+    while layers.iter().any(|&(depth, range)| (depth + delay) % (range * 2 - 2) == 0) {
+        delay += 1;
+    }
 
     delay
 }
