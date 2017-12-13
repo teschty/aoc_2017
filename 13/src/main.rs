3c1d63407503c6289b3f042c48771d6e431d2cbd
Tyler Tesch <tytesch@gmail.com>
2017-12-13 10:30:54 -0600

    Finish day thirteen part two

--- 13/src/main.rs
+++ 13/src/main.rs
@@ -9,8 +9,22 @@ fn read_file() -> String {
     contents
 }
 
-fn part_one(input: &str) -> i32 {
+fn calculate_severity(layers: &Vec<(i32, i32)>, delay: i32) -> (i32, bool) {
     let mut severity = 0;
+    let mut caught = false;
+
+    for &(depth, range) in layers {
+        if (depth + delay) % (range * 2 - 2) == 0 {
+            caught = true;
+            severity += depth * range; 
+        }
+    }
+
+    (severity, caught)
+}
+
+fn get_layers(input: &str) -> Vec<(i32, i32)> {
+    let mut layers = vec![];
 
     for line in input.lines() {
         let parts: Vec<i32> = line
@@ -19,12 +33,25 @@ fn part_one(input: &str) -> i32 {
             .collect();
         
         let (depth, range) = (parts[0], parts[1]);
-        if depth % (range * 2 - 2) == 0 {
-            severity += depth * range; 
-        }
+        layers.push((depth, range));
     }
 
-    severity
+    layers
+}
+
+fn part_one(input: &str) -> i32 {
+    calculate_severity(&get_layers(input), 0).0
+}
+
+fn part_two(input: &str) -> i32 {
+    let layers = get_layers(input);
+    let mut delay = 0;
+
+    // might be possible to calculate this in a smart way
+    // but let's just brute force it
+    while calculate_severity(&layers, delay).1 { delay += 1; }
+
+    delay
 }
 
 fn main() {
@@ -32,4 +59,5 @@ fn main() {
     let puzzle = puzzle.trim();
 
     println!("Solution to part one is {}", part_one(puzzle));
+    println!("Solution to part two is {}", part_two(puzzle));
 }
